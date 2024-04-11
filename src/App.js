f60bfb976ebaf641afd7c1a2d982a3b1819561ef
Seytkumar_Daniyar <seytkumar980@gmail.com>
2024-04-11 17:42:11 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import Circle from "./components/Circle/Circle";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      Hello ITCg
-    </div>
-  );
+class App extends Component {
+
+  random = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  state = {
+    numbers: [
+      {number: 10},
+      {number: 20},
+      {number: 30},
+      {number: 40},
+      {number: 50},
+    ]
+  }
+
+  changeNumber = () => {
+    console.log('Click');
+    const numbersCopy = this.state.numbers;
+    numbersCopy[0].number = this.random(5, 36);
+    numbersCopy[1].number = this.random(5, 36);
+    numbersCopy[2].number = this.random(5, 36);
+    numbersCopy[3].number = this.random(5, 36);
+    numbersCopy[4].number = this.random(5, 36);
+
+    for (let i = 0; i < numbersCopy.length; i++) {
+      for (let j = i + 1; j < numbersCopy.length; j++) {
+        while (numbersCopy[i].number === numbersCopy[j].number) {
+         numbersCopy[j].number = this.random(5, 36) 
+        };
+      };
+    };
+
+    const comparer = (a, b) => {
+      return a.number - b.number
+    };
+
+    numbersCopy.sort(comparer);
+
+    this.setState({numbers: numbersCopy});
+  }
+
+  
+
+  render() {
+    return (
+      <div className="App">
+        <div className="CircleContainer">
+          <Circle numberl={this.state.numbers[0].number}></Circle>
+          <Circle numberl={this.state.numbers[1].number}></Circle>
+          <Circle numberl={this.state.numbers[2].number}></Circle>
+          <Circle numberl={this.state.numbers[3].number}></Circle>
+          <Circle numberl={this.state.numbers[4].number}></Circle>
+        </div>
+      <button onClick={this.changeNumber} className="btn">Поменять цифры</button>
+      </div>
+    )
+  } 
 }
 
 export default App;
